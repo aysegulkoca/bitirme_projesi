8bc3bf6fa59af1d3f2ef7abdb8751af9912e9f38
Ayşegül Koca <aysegul.koca@bil.omu.edu.tr>
2019-03-22 17:19:16 +0300

    database'e veri ekle

--- scannetwork/models.py
+++ scannetwork/models.py
@@ -7,10 +7,16 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200, verbose_name="product_name")
     product_service = models.CharField(max_length=200, verbose_name="product_service")
 
+    def __str__(self):
+        return self.product_name
+
 
 class Version(models.Model):
     version = models.CharField(max_length=200, verbose_name="version")
     product = models.ForeignKey("Product", verbose_name="product_id", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.version
 
 
 class Vulnerability(models.Model):
@@ -19,3 +25,6 @@ class Vulnerability(models.Model):
     solution = models.TextField(verbose_name="solution")
     code = models.TextField(verbose_name="code")
     version = models.ForeignKey("Version", verbose_name="version_id", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.vulnerability
