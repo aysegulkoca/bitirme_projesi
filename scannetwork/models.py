709b92b31348738c4d26caef3656a6801b4e26ff
Ayşegül Koca <aysegul.koca@bil.omu.edu.tr>
2019-03-22 14:32:49 +0300

    zafiyetler için tabloları oluştur ve projeyi düzenli hale getir

---
+++ scannetwork/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, verbose_name="product_name")
+    product_service = models.CharField(max_length=200, verbose_name="product_service")
+
+
+class Version(models.Model):
+    version = models.CharField(mas_length=200, verbose_name="version")
+    product = models.ForeignKey("Product", verbose_name="product_id")
+
+
+class Vulnerability(models.Model):
+    vulnerability = models.CharField(max_length=200, verbose_name="vulnerability")
+    description = models.TextField(verbose_name="description")
+    solution = models.TextField(verbose_name="solution")
+    code = models.TextField(verbose_name="code")
+    version = models.ForeignKey("Version", verbose_name="version_id")
---
+++ scannetwork/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import nmap
+
+# Create your views here.
+
+
+def nmapscan(ip):
+    nmapresult = nmap.PortScanner()
+    nmapresult.scan(ip, arguments='-sV --script firewall-bypass')
+
+    result = ""
+    for host in nmapresult.all_hosts():
+        blank = ""
+        prototocols = nmapresult[host].all_protocols()
+        for protocoll in prototocols:
+            ports = nmapresult[host][protocoll].keys()
+            for port in ports:
+                if nmapresult[host][protocoll][port]['state'] == "open":
+                    name = nmapresult[host][protocoll][port]['name']
+                    version = nmapresult[host][protocoll][port]['version']
+                    blank = blank + ' \n port : %s\tname : %s\tversion : %s' % (port, name, version)
+        forhost = "\n" + host + " ipli cihaz için: " + blank
+        result = result + forhost
+
+    response = u"Nmap tarama sonucu: %s\n" % result
+    return response
+
