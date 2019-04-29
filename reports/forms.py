5726b0fc2bac6a67ceb49a4df632913c827c9ea2
Ayşegül Koca <aysegul.koca@bil.omu.edu.tr>
2019-04-29 11:26:28 +0300

    hatayı düzelt ve son halini güncelle

--- reports/forms.py
+++ reports/forms.py
@@ -4,6 +4,17 @@ from .models import Report
 
 
 class ReportForm(forms.ModelForm):
+    ip = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+
     class Meta:
         model = Report
         fields = [
