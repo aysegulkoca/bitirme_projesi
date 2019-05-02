603cba00af9cd2e9e94aee5589e44846d72df9cc
ESRA ODACI <esra.odaci@bil.omu.edu.tr>
2019-05-02 14:42:58 +0300

    xss iyileştirildi

--- scanweb/views.py
+++ scanweb/views.py
@@ -5,7 +5,6 @@ import requests as req
 import re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
-import cgi
 
 def scan_web(request):
     if request.method == 'POST':
@@ -15,24 +14,33 @@ def scan_web(request):
             title = form.cleaned_data.get('title')
             sqlInj = sql_injection(url)
             xssVuln = xss(url)
-        return render(request, 'templates/scanweb/scanweb_detail.html', {'sqlInj': sqlInj, 'xssVuln':xssVuln})
+            bs4(url)
+        return render(request, 'templates/scanweb/scanweb_detail.html', {
+            'url': url,
+            'title':title,
+            'sqlInj': sqlInj,
+            'xssVuln':xssVuln
+        })
     else:
         form = WebForm()
         return render(request, 'templates/scanweb/scanweb.html', {'form': form})
 
-
 def sql_injection(url):
     newUrl = ''
     total = 0
-
+    dic = bs4(url)
     queries = open("scanweb\queries.txt")
     data = queries.readlines()
-
     for i in range(len(data)):
         query = data[i]
         newUrl = url + query
         resp = req.get(newUrl)
         source = BeautifulSoup(resp.content, 'html.parser')
+        newDic = bs4(newUrl)
+        if dic==newDic:
+            continue
+        else:
+            total += 1
 
         if source == " ":
             total += 1
@@ -45,28 +53,48 @@ def sql_injection(url):
                 total += 1
 
     if total == 0:
-        output = "SQL Injection zafiyeti barındırmıyor!"
+        output = "barındırmıyor."
     else:
-        output = "SQL Injection zafiyeti barındırıyor olabilir. url:" + newUrl
+        output = "BARINDIRIYOR!"
 
     return output
 
 def xss(url):
+    xss = 0
+    payload = open("scanweb/payload.txt",encoding="utf8")
+
+    value = payload.readlines()
     resp = req.get(url)
     soup = BeautifulSoup(resp.content,'html.parser')
+
+    # Web Sayfalarındaki doldurulan alanalarda XSS denemesi
     fields = {}
-
     for input in soup.findAll('input'):
-        if input['type'] in ('text', 'hidden', 'password', 'submit', 'image'):
+        if input['type'] in ('text', 'hidden', 'password', 'image'):
+            for i in range(len(value)):
+                fields[input['name']] = value[i]
+                requests = req.post(url, fields)
+                soup2 = BeautifulSoup(requests.content, 'html.parser')
+                if value in soup2 :
+                    xss += 1
+    #Url üzerinden XSS denemesi
+    for j in range(len(value)):
+        val = value[j]
+        xssUrl = url + val
+        response = req.get(xssUrl)
+        soup3 = BeautifulSoup(response.content,'html.parser')
+        if val in soup3:
+            xss += 1
+    if xss == 0:
+        xssOutput = "barındırmıyor"
+    else:
+        xssOutput = "BARINDIRIYOR!"
+    return xssOutput
 
-            value = '<script>alert("XSS")</script>'
+def bs4(url):
+    dic = {}
+    resp = req.get(url)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    dic =  soup.find_all("td")
 
-            if 'value' in input:
-                value = input['value']
-            fields[input['name']] = value
-
-    for textarea in soup.findAll('textarea'):
-        fields[textarea['name']] = textarea.string or ''
-
-    a = requests.post(url,fields)
-    return a
+    return dic
